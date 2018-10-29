6ba7153ab6de99c494795045b92e86eba6ff6721
Quang Hung <quanghungleo@gmail.com>
2018-10-29 12:21:25 +0700

    First

---
+++ DemoWebDotNet/Controllers/StudentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebDotNet.Controllers
+{
+    public class StudentController : Controller
+    {
+
+        [HttpGet("/create")]
+        public IActionResult CreateStudent()
+        {
+            return View();
+        }
+    }
+}
---
+++ DemoWebDotNet/Models/ContextDatabase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoWebDotNet.Models
+{
+    public class ContextDatabase : DbContext
+    {
+        public ContextDatabase(DbContextOptions<ContextDatabase> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentViewModel> Students { get; set; }
+    }
+}
